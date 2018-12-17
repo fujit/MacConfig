d123acff0fb0239cdf446006e1a4c945ed0f473f
ayuzaka <ayuzaka.f@gmail.com>
2018-12-17 23:49:00 +0900

    add new app

--- appInstall.sh
+++ appInstall.sh
@@ -9,23 +9,25 @@ echo "Appをインストール"
 
 # general
 brew cask install google-chrome
+brew cask install firefox
 brew cask install alfred
 brew cask install appcleaner
 brew cask install kindle
 brew cask install slack
-brew cask install firefox
 brew cask install keka
 brew cask install qblocker
 brew cask install amazon-music
 brew cask install amazon-drive
 brew cask install music-manager
 brew cask install google-backup-and-sync
+brew cask install google-japanese-ime
 brew cask install 1password
 brew cask install clipy
 brew cask install fluid
 brew cask install calibre
 brew cask install flux
 brew cask install cheatsheet
+brew cask install skitch
 
 # dev
 brew cask install atom
