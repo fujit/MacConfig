ca9fa7956473dfb09c92553903d608b9ad03e92e
ayuzaka <ayuzaka.f@gmail.com>
2020-06-06 13:31:28 +0900

    add brew bundle

--- init.sh
+++ init.sh
@@ -3,77 +3,13 @@
 # Homebrewをインストール
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew tap caskroom/cask1
 
-# brewcaskのダウンロード先を/Applicationsに変更
-echo export HOMEBREW_CASK_OPTS="--appdir=/Applications" >> ~/.bashrc
-source ~/.bashrc
+cp ./.Brewfile ~/
+brew bundle --global
 
-touch ~/.zshrc
-echo export HOMEBREW_CASK_OPTS="--appdir=/Applications" >> ~/.zshrc
-source ~/.zshrc
-
-####################
-# App
-####################
-
-# general
-brew cask install google-chrome
-brew cask install firefox
-brew cask install bitwarden
-brew cask install alfred
-brew cask install appcleaner
-brew cask install clipy
-brew cask install google-japanese-ime
-brew cask install slack
-brew cask install authy
-brew cask install hyperswitch
-brew cask install spotify
-brew cask install notion
-brew cask install keka
-
-# developer
-brew cask install docker
-brew cask install iterm2
-brew cask install virtualbox
-brew cask install visual-studio-code
-
-brew install yarn
-brew install git
-brew install neovim
-brew install rmtrash
-brew install tree
-brew install zshrc
-brew install zplug
-brew install bat
-brew install textql
-brew install fzf
-brew install exa
-brew install ripgrep
-brew install fd
-brew install starship
-brew install mycli
-brew install pgcli
-
-# Mac app
-brew install mas
-mas install 497799835  # Xcode
-mas install 568494494  # Pocket
-mas install 539883307  # LINE
-
-####################
-# font
-####################
 # Ricty
-brew tap sanemat/font
-brew install ricty
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
-
-brew tap caskroom/fonts
-brew cask install font-myrica
-brew cask install font-source-han-code-jp
-brew cask install font-fira-code
 
 # nerd 用のフォント
 brew cask install font-hack-nerd-font
@@ -92,23 +28,8 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/.cache/dein
 
 ####################
-# Go
-####################
-brew install go
-
-####################
 # Node.js
 ####################
-brew install nodebrew
 mkdir -p ~/.nodebrew/src
 nodebrew install-binary latest
 nodebrew use `nodebrew ls`
-
-yarn global add \
-  sort-package-json \
-  markdownlint
-
-####################
-# Python
-####################
-brew install pyenv
