cb02565dc0d141541cb7aa3f0dcd3120da935706
ayuzaka <ayuzaka.f@gmail.com>
2021-09-12 12:41:02 +0900

    Update setting

--- init.sh
+++ init.sh
@@ -1,38 +1,5 @@
 #!/bin/sh
 
-# Homebrewをインストール
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-
-cp ./.Brewfile ~/
-brew bundle --global
-
-# Ricty
-cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
-
-# nerd 用のフォント
-cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-
-# 標準シェルをzshに変更
-echo /usr/local/bin/zsh >> /etc/shells
-chsh -s /usr/local/bin/zsh
-
-####################
-# Vim
-####################
-
-# dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
-
-####################
-# Node.js
-####################
-mkdir -p ~/.nodebrew/src
-nodebrew install-binary latest
-nodebrew use `nodebrew ls`
-
 ####################
 # MacOS
 ####################
