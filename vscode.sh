3d85e51566434807aaff6ef06bfbad0e69cf5e40
ayuzaka <ayuzaka.f@gmail.com>
2021-09-12 12:41:07 +0900

    Update VSCode plugin setting

--- vscode.sh
+++ vscode.sh
@@ -9,10 +9,7 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension shardulm94.trailing-spaces
 code --install-extension mosapride.zenkaku
 code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension dtsvet.vscode-wasm
-code --install-extension ms-azuretools.vscode-docker
 code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension eamodio.gitlens
 
 # Markdown
 code --install-extension DavidAnson.vscode-markdownlint
@@ -21,4 +18,3 @@ code --install-extension yzhang.markdown-all-in-one
 
 # Theme
 code --install-extension harg.iceberg
-code --install-extension dracula-theme.theme-dracula
